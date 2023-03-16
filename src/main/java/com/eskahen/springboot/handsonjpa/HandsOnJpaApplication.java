f6eeded07d0a01f818a87acf16045917784e7fd7
Lionel Traore <hello@lioneltraore.com>
2023-03-16 14:47:06 +0000

    first commit

---
+++ src/main/java/com/eskahen/springboot/handsonjpa/HandsOnJpaApplication.java
@@ -0,0 +1,13 @@
+package com.eskahen.springboot.handsonjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnJpaApplication.class, args);
+	}
+
+}
